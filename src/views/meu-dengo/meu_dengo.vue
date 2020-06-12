69e984dfbc7942af37fe694597b973ed8646bbe1
sergio ramos <sfilhu@gmail.com>
2020-06-12 00:21:59 -0300

    new version

--- src/views/meu-dengo/meu_dengo.vue
+++ src/views/meu-dengo/meu_dengo.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        <h1>Meu Dengo</h1>
+    <div style="width: 100vw; height: 100vh; background-color: #000;color: #fff">
+        <h1 style="margin: 15px 0;text-align: center;">Meu Dengo 🌻</h1>
+        <iframe 
+            width="100%" 
+            height="700"
+            src="https://www.youtube.com/embed/9z--9Y1JFYg?autoplay=1&rel=0" 
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen
+        ></iframe>
     </div>
 </template>
 
--- src/views/my-message/my_message.vue
+++ src/views/my-message/my_message.vue
@@ -1,11 +1,43 @@
 <template>
-    <div>
-        <h1>My Message</h1>
+    <div style="width: 100vw; height: 100vh; background-color: #000;color: #fff">
+        <h1 style="margin: 15px 0;text-align: center;">Minha mensagem 🌷</h1>
+
+        <v-carousel height="700">
+            <v-carousel-item
+              v-for="(item,i) in items"
+              :key="i"
+              reverse-transition="fade-transition"
+              transition="fade-transition"
+            >
+                <iframe 
+                    width="100%" 
+                    height="700"
+                    :src="`https://www.youtube.com/embed/${item}?autoplay=1&rel=0`" 
+                    frameborder="0"
+                    allow="accelerometer; autoplay; gyroscope; picture-in-picture" 
+                    allowfullscreen
+                ></iframe>
+            </v-carousel-item>
+          </v-carousel>
+        
     </div>
 </template>
 
 <script>
     export default {
         name: 'my-message',
+        data: () => {
+            return {
+                items : [
+                    'jfCeAXBHYSw',
+                    'ge6uPA19FNY',
+                    'ChJWCVUnoSk',
+                    'o7Q5aMg1ywY',
+                    'wJdYEuU8G1Q',
+                    'u1CDIzfJybo',
+                    'hGoDEhSjbKs'
+                ]
+            }
+        },
     }
 </script>
